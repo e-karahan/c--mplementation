6357457fc4d60edf50e8e734b78bcdcde4235702
e-karahan <emirhankarahan37@gmail.com>
2020-04-26 01:50:24 +0300

    mycat Implementation

---
+++ mycp/mycp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFF_SIZE 1024
+
+int main(int argc, char* argv[])
+{
+	int srcFD,destFD,nbread,nbwrite;
+	char *buff[BUFF_SIZE];
+
+	/*Source ve Destination Dosyalarına Ulaşıldımı Kontrolu Yapılır Ulaşılamadıysa Hata Yazdırılır*/
+	if(argc != 3 || argv[1] == "--help")
+	{
+		printf("\nUsage: cpcmd source_file destination_file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*Source Dosyası Açılıyor*/
+	srcFD = open(argv[1],O_RDONLY);
+  	/*Dosya Açılamadıysa Hata Yazdırılıyor*/
+	if(srcFD == -1)
+	{
+		printf("\nError opening file %s errno = %d\n",argv[1],errno);
+		exit(EXIT_FAILURE);
+	}
+
+	  /*Destination Dosyası Açılıyor*/
+	destFD = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    /*Dosya Açılamadıysa Hata Yazdırılıyor*/
+	if(destFD == -1)
+	{
+		printf("\nError opening file %s errno = %d\n",argv[2],errno);
+		exit(EXIT_FAILURE);
+	}
+
+	/*Data transferine EOF'ye(Dosyanın Sonuna) ulaşana kadar devam edilir*/
+	while((nbread = read(srcFD,buff,BUFF_SIZE)) > 0)
+	{
+		if(write(destFD,buff,nbread) != nbread)
+			printf("\nError in writing data to %s\n",argv[2]);
+	}
+/*Datanın okunmasında hata var*/
+	if(nbread == -1)
+		printf("\nError in reading data from %s\n",argv[1]);
+/*Source Dosyası Kapatılamadı*/
+	if(close(srcFD) == -1)
+		printf("\nError in closing file %s\n",argv[1]);
+/*Destination Dosyası Kapatılamadı*/
+	if(close(destFD) == -1)
+		printf("\nError in closing file %s\n",argv[2]);
+
+	exit(EXIT_SUCCESS);
+}
